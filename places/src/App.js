63ce3f6959151874c975f85a54b29b46ea0bf73d
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-08-27 16:49:56 -0500

    20200827: We have created a basic structure for our app

--- places/src/App.js
+++ places/src/App.js
@@ -2,26 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
+import { hola } from './components/Title';
 
 function App() {
   return (
-    <div className="App">
-      <Title></Title>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <div>
+          <Title></Title>
+          <button>Crear cuenta gratuita</button>
+          <div>
+             <ul>
+               <li>
+                 <h3>Calificaciones con emiciones</h3>
+                 <p>Califica tus lugares con experiencias, no con números</p>
+               </li>
+               <li>
+                 <h3>¿Sin internet? Sin problemas</h3>
+                 <p>Places funciona sin internet o en conexiones lentas</p>
+               </li>
+               <li>
+                 <h3>Yus lugares favoritos</h3>
+                 <p>Define tus sitios de lugares favoritos</p>
+               </li>
+             </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- places/src/components/Title.js
+++ places/src/components/Title.js
@@ -12,3 +12,4 @@ export default class Title extends React.Component {
     
    }
 }
+
