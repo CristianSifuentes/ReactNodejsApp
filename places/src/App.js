bb5d985ee5101af43639cd78f16086284451f4f3
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-04 15:44:28 -0500

    20200904: We changed the desing for implement cards using a new funciont returning html code with a new information called from js file like a api response

--- places/src/App.js
+++ places/src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import { Card, CardText } from 'material-ui/Card'
+import { Card, CardText, CardMedia, CardTitle } from 'material-ui/Card'
 import { indigo400, redA400, lightBlueA400, amberA400 }  from  'material-ui/styles/colors';
 import './App.css';
 import Title from './components/Title';
-import { hola } from './components/Title';
+// import { hola } from './components/Title';
+
+import data from './request/places';
 
 class App extends Component {
   
+  constructor(props){
+    super(props);
+  }
+
+  places() {
+    return data.places.map(places => {
+      return (
+        <div className="col-xs-12 col-sm-4">
+       <Card>
+          <CardMedia>
+            <img src={process.env.PUBLIC_URL + places.imagenUrl}/>
+          </CardMedia>
+          <CardTitle title={places.title}>
+
+          </CardTitle>
+          <CardText>{places.descripcion}</CardText>
+      </Card>
+        </div>
+      )
+    }) 
+  }
   render() {
   return (
     <MuiThemeProvider>
@@ -23,63 +46,68 @@ class App extends Component {
 
           <ul>
 
-<Card className="Header-Benefit">
-  
-  <CardText>
-    <div className="row">
-      <div className="Header-Benefit-image" style={{ "background-color" : redA400 }}>
-          <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
-      </div>
-      <div className="Header-Benefit-content">
-          <h3>Calificaciones con emiciones</h3>
-          <p>Califica tus lugares con experiencias, no con números</p>
-      </div>
-    </div>
+              <Card className="Header-Benefit">
+                
+                <CardText>
+                  <div className="row">
+                    <div className="Header-Benefit-image" style={{ "background-color" : redA400 }}>
+                        <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
+                    </div>
+                    <div className="Header-Benefit-content">
+                        <h3>Calificaciones con emiciones</h3>
+                        <p>Califica tus lugares con experiencias, no con números</p>
+                    </div>
+                  </div>
 
-  </CardText>
-</Card>
-
-
-<Card className="Header-Benefit">
-  
-  <CardText>
-    <div className="row">
-      <div className="Header-Benefit-image" style={{ "background-color" : lightBlueA400 }}>
-          <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
-      </div>
-      <div className="Header-Benefit-content">
-         <h3>¿Sin internet? Sin problemas</h3>
-         <p>Places funciona sin internet o en conexiones lentas</p>
-      </div>
-    </div>
-
-  </CardText>
-</Card>
+                </CardText>
+              </Card>
 
 
-<Card className="Header-Benefit">
-  
-  <CardText>
-    <div className="row">
-      <div className="Header-Benefit-image" style={{ "background-color" : amberA400 }}>
-          <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
-      </div>
-      <div className="Header-Benefit-content">
-         <h3>Yus lugares favoritos</h3>
-       <p>Define tus sitios de lugares favoritos</p>
-      </div>
-    </div>
+              <Card className="Header-Benefit">
+                
+                <CardText>
+                  <div className="row">
+                    <div className="Header-Benefit-image" style={{ "background-color" : lightBlueA400 }}>
+                        <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
+                    </div>
+                    <div className="Header-Benefit-content">
+                      <h3>¿Sin internet? Sin problemas</h3>
+                      <p>Places funciona sin internet o en conexiones lentas</p>
+                    </div>
+                  </div>
 
-  </CardText>
-</Card>
+                </CardText>
+             
+               </Card>
 
-</ul>
+
+              <Card className="Header-Benefit">
+                
+                <CardText>
+                  <div className="row">
+                    <div className="Header-Benefit-image" style={{ "background-color" : amberA400 }}>
+                        <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
+                    </div>
+                    <div className="Header-Benefit-content">
+                       <h3>¿Sin internet? Sin problemas</h3>
+                       <p>Places funciona sin internet o en conexiones lentas</p>
+                    </div>
+                  </div>
+
+                </CardText>
+              
+              </Card>
+
+          </ul>
             
           </div>        
         </div>
       </div>
-      <div  style={{ "background-color": indigo400, "padding": "50px"  }}>
-             
+      <div  style={{ "background-color": indigo400, "padding": "50px", "color":"white"  }}>
+             <h3 style={{ 'fontSize':'24px' }}>Sitios populares</h3>
+             <div className="row">
+                {this.places()}
+             </div>
       </div>
 
     </MuiThemeProvider>
---
+++ places/src/request/places.js
@@ -0,0 +1,20 @@
+export default {
+    places: [
+        {
+            "imagenUrl":"/images/place-3.png",
+            "title":"Lugar 3",
+            "descripcion":"Descripcion para lugar numero 1"
+            },
+            {
+            "imagenUrl":"/images/place-2.png",
+            "title":"Lugar 2",
+            "descripcion": "Descripcion para lugar numero 2"
+            },
+            {
+            "imagenUrl":"/images/place-1.png",
+            "title":"Lugar 1",
+            "descripcion": "Descripcion para lugar numero 3"
+            }
+    ]
+    
+}
