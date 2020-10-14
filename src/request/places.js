7294d22866e2b276c9e6a0fa0a22f606779f7b08
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-10-14 13:57:46 -0500

    20201014: Finished placehorizontal

--- src/request/places.js
+++ src/request/places.js
@@ -1,19 +1,24 @@
 export default {
     places: [
         {
-            "imagenUrl":"/images/place-3.png",
+            "imagenUrl":"/images/place-3.jpg",
             "title":"Lugar 3",
-            "descripcion":"Descripcion para lugar numero 1"
+            "descripcion":"Descripcion para lugar numero 1",
+            "address": 'Av principal 1'
             },
             {
-            "imagenUrl":"/images/place-2.png",
+            "imagenUrl":"/images/place-2.jpg",
             "title":"Lugar 2",
-            "descripcion": "Descripcion para lugar numero 2"
+            "descripcion": "Descripcion para lugar numero 2",
+            "address": 'Av principal 2'
+
             },
             {
-            "imagenUrl":"/images/place-1.png",
+            "imagenUrl":"/images/place-1.jpg",
             "title":"Lugar 1",
-            "descripcion": "Descripcion para lugar numero 3"
+            "descripcion": "Descripcion para lugar numero 3",
+            "address": 'Av principal 3'
+
             }
     ]
     
